c6b316b9606ef00d4ea7a0fb7f0681801731ab0d
David Lee <javaugi@yahoo.com>
2019-03-27 22:36:08 -0400

    added auth code

--- src/main/java/com/spring5/payroll/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/spring5/payroll/ReactAndSpringDataRestApplication.java
@@ -22,11 +22,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Greg Turnquist
  */
 // tag::code[]
-@SpringBootApplication
+//@SpringBootApplication
 public class ReactAndSpringDataRestApplication {
 
+    /*
     public static void main(String[] args) {
         SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
     }
+    // */
 }
 // end::code[]
