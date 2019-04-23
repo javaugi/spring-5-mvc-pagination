614a7776ef766ab9348fa01060802762a4534ec4
David Lee <david@ciminc.com>
2019-04-23 16:28:42 -0400

    added test method to convert list to sql in clause

---
+++ src/test/java/com/spring5/MyTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 Center for Information Management, Inc.
+ *
+ * This program is proprietary.
+ * Redistribution without permission is strictly prohibited.
+ * For more information, contact <http://www.ciminc.com>
+ */
+package com.spring5;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.util.StringUtils;
+
+/**
+ *
+ *
+ * @author david
+ * @version $LastChangedRevision $LastChangedDate Last Modified Author:
+ * $LastChangedBy
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class MyTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MyTest.class);
+
+    @Test
+    public void contextLoads() {
+        List<String> list = new ArrayList();
+        list.add("Test");
+        list.add("Test2");
+        list.add("Test3");
+
+        String inClause = "(" + StringUtils.join(list, ",").replaceAll("(\\w+)", "'$1'") + ")";
+        log.info("inClause {}", inClause);
+    }
+}
